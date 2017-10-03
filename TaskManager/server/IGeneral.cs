6761450d13e99b954097120798e68959132b5508
vladimir <student@K202-14>
2017-10-03 21:43:20 +0300

    WCF

---
+++ TaskManager/server/IGeneral.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    [ServiceContract]
+    interface IGeneral
+    {
+        [OperationContract]
+        Result Login(string email, string password);
+        [OperationContract]
+        Result SignUp(string email, string password, string name);
+    }
+}
