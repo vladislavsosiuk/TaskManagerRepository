f85b42e6c2f62455a7c75f3e37819eb1bba100de
vladimir <student@K202-14>
2017-10-05 19:48:25 +0300

    none

--- TaskManager/server/General.cs
+++ TaskManager/server/General.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using DataLair;
-using DataLair.DataModel;
 using System.Text.RegularExpressions;
 using server.BusinessLayer;
 
