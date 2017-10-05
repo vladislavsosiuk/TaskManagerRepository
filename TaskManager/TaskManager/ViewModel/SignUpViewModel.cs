f650e50ed2d34f9f27fd7cf86dd939d2b3c220f7
stas <student@K202-13>
2017-10-05 21:45:36 +0300

    last

---
+++ TaskManager/TaskManager/ViewModel/SignUpViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TaskManager.Helpers;
+using server;
+
+namespace TaskManager.ViewModel
+{
+    class SignUpViewModel : INotifyPropertyChanged
+    {
+        public SignUpViewModel(LoginView view)
+        {
+            CurrentPage = view;
+        }
+        Command signUpCommandReg;
+        bool isLoading;
+        public LoginView CurrentPage
+        {
+            get; set;
+        }
+        
+        public bool IsLoading
+        {
+            get
+            {
+                return isLoading;
+            }
+            set
+            {
+                isLoading = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand SignUpCommandReg
+        {
+            get
+            {
+                if (signUpCommandReg == null)
+                    signUpCommandReg = new Command(SignUpReg);
+                return signUpCommandReg;
+            }
+        }
+        public void SignUpReg(object parameter)
+        {
+            
+        }
+        private void OnPropertyChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
