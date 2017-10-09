0448386912d2a52c0ea3f8dc291108c02af91277
vladimir <student@K202-14>
2017-10-09 19:59:01 +0300

    added advanced check for entered data

--- TaskManager/server/General.cs
+++ TaskManager/server/General.cs
@@ -15,62 +15,65 @@ namespace server
 
         public BusinessUser Login(string email, string password)
         {
-
+            if(CheckPass(password))
+                return new BusinessUser { Result = new Result(-1, "Password does not match the requirements!") };
+            if (CheckPass(email))
+                return new BusinessUser { Result = new Result(-2, "Email does not match the requirements!") };
             var users = context.Users.Where(u => u.Email == email && u.Password == password).ToList();
-
-            if (users != null && users.Count > 0 && CheckPass(password) && CheckEmailAddress(email))
+            if (users != null && users.Count > 0)
             {
                 var user = users.FirstOrDefault();
-                return new BusinessUser { Name = user.Name };
+                return new BusinessUser { Name = user.Name, Password = user.Password, Email = user.Email, UserID=user.UserID, Tasks=user.Tasks};
             }
-            return new BusinessUser { Result = new Result ( -1, "user not found!" )};
+            return new BusinessUser { Result = new Result(-3, "Login is not succefull, something going wrong!") };
         }
 
         public BusinessUser SignUp(string email, string password, string name)
         {
+            if (CheckPass(password))
+                return new BusinessUser { Result = new Result(-1, "Password does not match the requirements!") };
+            if (CheckPass(email))
+                return new BusinessUser { Result = new Result(-2, "Email does not match the requirements!") };
+            if (CheckName(name))
+                return new BusinessUser { Result = new Result(-3, "Name does not match the requirements!") };
             var user = new User() { Name = name, Email = email, Password = password };
-            if (user.Name != null && user.Email != null && user.Password != null&& CheckPass(password)&& CheckEmailAddress(email)&& CheckEmailAddress(name))
+            if (user.Name != null && user.Email != null && user.Password != null)
             {
                 context.Users.Add(user);
                 context.SaveChanges();
-                return new BusinessUser { Name = user.Name, Password = user.Password, Email = user.Email };
+                return new BusinessUser { Name = user.Name, Password = user.Password, Email = user.Email, UserID = user.UserID, Tasks = user.Tasks};
             }
-            return new BusinessUser { Result = new Result(-1, "Registration is not succefull, something wrong with data") };
+            return new BusinessUser { Result = new Result(-4, "Registration is not succefull, something going wrong!") };
 
 
         }
-        public Result ForgotPassword(string email)
-        {
-
-            var users = context.Users.Where(u => u.Email == email).ToList();
-            
-            if (users!=null && users.Count == 1&&CheckEmailAddress(email))
-            {
-               string pass= users.FirstOrDefault().Password;
-                  return new Result(1, pass);
-            }
-            return new Result(-1, "User not found");
-        }
+    
         public List<MyTask> ActualTasks(int userID)
         {
+
             var users = context.Users.Where(u => u.UserID == userID).ToList();
             
             if(users!=null&& users.Count == 1)
             {
                 var tasks = users.FirstOrDefault().Tasks;
-                return new List<MyTask>(tasks);
+                return tasks;
             }
-            return new List<MyTask>(null);
+            return null;
 
         }
         public bool CheckPass(string pass)
         {
-          int pass_lenght = pass.Length;
-           if (pass_lenght == 8 || pass_lenght > 8)
-                if (pass.Contains("(?!^[0-9]*$)(?!^[a-zA-Z]*$)^(.{8,15})$"))
-                    return true;
-            return false;
-       }
+            // от 6 до 15 символов
+            try
+            {
+                Regex rx = new Regex(@"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^(.{6,15})$");
+                return rx.IsMatch(pass);
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
       
         public static bool CheckEmailAddress(string email)
         {
@@ -87,30 +90,32 @@ namespace server
         }
         public bool CheckName(string name)
         {
-            int name_lenght = name.Length;
-            if (name_lenght == 2 || name_lenght > 2)
-                if (name.Contains("(?!^[a-zA-Z]*$)^(.{2,15})$"))
-                    return true;
-            return false;
-            
+            //Цифры в имени должны отсуствоватть, от 2 до 15 символов
+            try
+            {
+                Regex rx = new Regex(@"(?!^[a-zA-Z]*$)^(.{2,15})$");
+                return rx.IsMatch(name);
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+
         }
 
         public Result RemindPassword(string email)
         {
+            if(CheckEmailAddress(email))
+                return new Result(-1, "Email does not match the requirements!");
             var users = context.Users.Where(u => u.Email == email).ToList();
-
-            if (users != null && users.Count == 1 && CheckEmailAddress(email))
+            if (users != null && users.Count == 1 )
             {
                 string pass = users.FirstOrDefault().Password;
                 return new Result(1, pass);
             }
-            return new Result(-1, "User not found");
+            return new Result(-2, "User not found");
         }
 
-        //Result IGeneral.ActualTasks(int userID)
-        //{
-        //    throw new NotImplementedException();
-        //}
 
         public General()
         {
