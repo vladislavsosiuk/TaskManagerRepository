062572ad23747bc52dedd86c91c0cf491d52c65e
vladeslav <student@K202-16>
2017-10-10 21:31:56 +0300

    change vova's general cs and igeneral

---
+++ TaskManager/server/BusinessLayer/BusinessProject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace server.BusinessLayer
+{
+    public class BusinessProject
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        //Ведущий проекта
+        public  BusinessUser OwnerUser { get; set; }
+
+        //Список задач проекта
+        public List<BussinessMyTask> Tasks { get; set; }
+
+        public BusinessProject()
+        {
+            Tasks = new List<BussinessMyTask>();
+        }
+    }
+}
--- TaskManager/server/General.cs
+++ TaskManager/server/General.cs
@@ -15,17 +15,26 @@ namespace server
 
         public BusinessUser Login(string email, string password)
         {
-            if(CheckPass(password))
-                return new BusinessUser { Result = new Result(-1, "Password does not match the requirements!") };
-            if (CheckPass(email))
-                return new BusinessUser { Result = new Result(-2, "Email does not match the requirements!") };
             var users = context.Users.Where(u => u.Email == email && u.Password == password).ToList();
             if (users != null && users.Count > 0)
             {
                 var user = users.FirstOrDefault();
-                return new BusinessUser { Name = user.Name, Password = user.Password, Email = user.Email, UserID=user.UserID, Tasks=user.Tasks};
+                var userToReturn = new BusinessUser { Name = user.Name, Password = user.Password, Email = user.Email, UserID=user.UserID};
+                 var userTasks = context.MyTasks.Where(t => t.Observers.Contains(user)).Select(tt=>new BussinessMyTask
+                 {
+                     ID =tt.ID,
+                     CurrentPriority =tt.CurrentPriority,
+                     Description = tt.Description,
+                     Name = tt.Name,
+                     Prognosis = tt.Prognosis,
+                     TimeStart = tt.TimeStart,
+                     TimeStop = tt.TimeStop,                     
+                 }).ToList();
+                userToReturn.Tasks = userTasks;
+                return userToReturn;
+
             }
-            return new BusinessUser { Result = new Result(-3, "Login is not succefull, something going wrong!") };
+            return new BusinessUser { Result = new Result(-1, "User not found") };
         }
 
         public BusinessUser SignUp(string email, string password, string name)
@@ -37,25 +46,53 @@ namespace server
             if (CheckName(name))
                 return new BusinessUser { Result = new Result(-3, "Name does not match the requirements!") };
             var user = new User() { Name = name, Email = email, Password = password };
-            if (user.Name != null && user.Email != null && user.Password != null)
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            var userFromDb = context.Users.Where(u => u.Email == email && u.Password == u.Password).Select(ur => new BusinessUser
             {
-                context.Users.Add(user);
-                context.SaveChanges();
-                return new BusinessUser { Name = user.Name, Password = user.Password, Email = user.Email, UserID = user.UserID, Tasks = user.Tasks};
-            }
-            return new BusinessUser { Result = new Result(-4, "Registration is not succefull, something going wrong!") };
-
-
+                Email = ur.Email,
+                Name = ur.Name,
+                UserID = ur.UserID,
+                Tasks = new List<BussinessMyTask>(),
+            }).FirstOrDefault();
+            return userFromDb;
         }
     
-        public List<MyTask> ActualTasks(int userID)
+        public List<BussinessMyTask> ActualTasks(int userID)
         {
 
-            var users = context.Users.Where(u => u.UserID == userID).ToList();
+            var user = context.Users.Where(u => u.UserID == userID).FirstOrDefault();
             
-            if(users!=null&& users.Count == 1)
+            if(user!=null)
             {
-                var tasks = users.FirstOrDefault().Tasks;
+               var tasks= user.Tasks.Select(t => new BussinessMyTask
+                {
+                    CurrentPriority = t.CurrentPriority,
+                    Description = t.Description,
+                    ID = t.ID,
+                    Name = t.Name,
+                    Prognosis = t.Prognosis,
+                    TimeStart = t.TimeStart,
+                    TimeStop = t.TimeStop,
+                    Project = context.Projects.Where(p=>p.ID==t.ProjectID).Select(pp=>new BusinessProject
+                    {
+                        ID = pp.ID,
+                        Name=pp.Name,
+                        OwnerUser = context.Users.Where(u=>u.UserID== pp.OwnerUser.UserID).Select(uu=>new BusinessUser
+                        {
+                            Email=uu.Email,
+                            Name=uu.Name,
+                            UserID=uu.UserID,
+                        }).First(),                        
+                    }).FirstOrDefault(),
+                    ResponsibleUser = context.Users.Where(u=>u.UserID==t.ResponsibleUserID).Select(su=>new BusinessUser
+                    {
+                        Email=su.Email,
+                        Name = su.Name,
+                        UserID = su.UserID,
+                    }).FirstOrDefault(),
+                }).ToList();   
                 return tasks;
             }
             return null;
@@ -107,10 +144,10 @@ namespace server
         {
             if(CheckEmailAddress(email))
                 return new Result(-1, "Email does not match the requirements!");
-            var users = context.Users.Where(u => u.Email == email).ToList();
-            if (users != null && users.Count == 1 )
+            var user = context.Users.Where(u => u.Email == email).FirstOrDefault();
+            if(user!=null)
             {
-                string pass = users.FirstOrDefault().Password;
+                string pass = user.Password;
                 return new Result(1, pass);
             }
             return new Result(-2, "User not found");
