dfd659b586c5e06149c18fbc2e327a897a915a9c
vladimir <student@K202-14>
2017-10-19 19:30:15 +0300

    added something

--- TaskManager/server/MainViewContext.cs
+++ TaskManager/server/MainViewContext.cs
@@ -18,62 +18,63 @@ namespace server
         {
             Context = context;
         }
-        public List<BusinessProject> GetAllProjects()
-        {
+        #region methods
+        //public List<BusinessProject> GetAllProjects()
+        //{
 
-            var allProjects = Context.Projects.Select(p => new BusinessProject
-            {
-                ID = p.ID,
-                Name = p.Name,
-                OwnerUser = new BusinessUser
-                {
-                    Email = p.OwnerUser.Email,
-                    Name = p.OwnerUser.Name,
-                    UserID = p.OwnerUser.UserID,
-                },
-                Tasks = p.Tasks.Select(t => new BussinessMyTask
-                {
-                    CurrentPriority = t.CurrentPriority,
-                    Description = t.Description,
-                    ID = t.ID,
-                    IsDone = t.IsDone,
-                    Name = t.Name,
-                    Prognosis = t.Prognosis,
-                    ResponsibleUser = new BusinessUser
-                    {
-                        Email = t.ResponsibleUser.Email,
-                        Name = t.ResponsibleUser.Name,
-                        UserID = t.ResponsibleUser.UserID,
-                    },
-                    TimeStart = t.TimeStart,
-                    TimeStop = t.TimeStop,
-                }).ToList(),
-            });
-            return allProjects.ToList();
-        }
+        //    var allProjects = Context.Projects.Select(p => new BusinessProject
+        //    {
+        //        ID = p.ID,
+        //        Name = p.Name,
+        //        OwnerUser = new BusinessUser
+        //        {
+        //            Email = p.OwnerUser.Email,
+        //            Name = p.OwnerUser.Name,
+        //            UserID = p.OwnerUser.UserID,
+        //        },
+        //        Tasks = p.Tasks.Select(t => new BussinessMyTask
+        //        {
+        //            CurrentPriority = t.CurrentPriority,
+        //            Description = t.Description,
+        //            ID = t.ID,
+        //            IsDone = t.IsDone,
+        //            Name = t.Name,
+        //            Prognosis = t.Prognosis,
+        //            ResponsibleUser = new BusinessUser
+        //            {
+        //                Email = t.ResponsibleUser.Email,
+        //                Name = t.ResponsibleUser.Name,
+        //                UserID = t.ResponsibleUser.UserID,
+        //            },
+        //            TimeStart = t.TimeStart,
+        //            TimeStop = t.TimeStop,
+        //        }).ToList(),
+        //    });
+        //    return allProjects.ToList();
+        //}
 
-        public List<BussinessMyTask> GetAllTasks()
-        {
-            var allTasks = Context.MyTasks.Select(t => new BussinessMyTask
-            {
-                CurrentPriority = t.CurrentPriority,
-                Description = t.Description,
-                ID = t.ID,
-                IsDone = t.IsDone,
-                Name = t.Name,
-                Prognosis = t.Prognosis,
-                ResponsibleUser = new BusinessUser
-                {
-                    Email = t.ResponsibleUser.Email,
-                    Name = t.ResponsibleUser.Name,
-                    UserID = t.ResponsibleUser.UserID,
-                },
-                TimeStart = t.TimeStart,
-                TimeStop = t.TimeStop,
-            }).ToList();
-         return allTasks.ToList();
-        }
-
+        //public List<BussinessMyTask> GetAllTasks()
+        //{
+        //    var allTasks = Context.MyTasks.Select(t => new BussinessMyTask
+        //    {
+        //        CurrentPriority = t.CurrentPriority,
+        //        Description = t.Description,
+        //        ID = t.ID,
+        //        IsDone = t.IsDone,
+        //        Name = t.Name,
+        //        Prognosis = t.Prognosis,
+        //        ResponsibleUser = new BusinessUser
+        //        {
+        //            Email = t.ResponsibleUser.Email,
+        //            Name = t.ResponsibleUser.Name,
+        //            UserID = t.ResponsibleUser.UserID,
+        //        },
+        //        TimeStart = t.TimeStart,
+        //        TimeStop = t.TimeStop,
+        //    }).ToList();
+        // return allTasks.ToList();
+        //}
+        #endregion
         public List<BusinessUser> GetAllUsers()
         {
             var allUsers = Context.Users.Select(u => new BusinessUser
@@ -121,8 +122,8 @@ namespace server
         }
 
         public List<BussinessMyTask> GetTasksByUserID(int userID)
-        {
-            var TasksById = Context.MyTasks.Where(t=>t.ResponsibleUserID== userID).Select(t => new BussinessMyTask
+        {     
+            var TasksById = Context.MyTasks.Where(t=>t.UsersThatWorksOnThatTask.Any(u=>u.UserID==userID)).Select(t => new BussinessMyTask
             {
                 CurrentPriority = t.CurrentPriority,
                 Description = t.Description,
